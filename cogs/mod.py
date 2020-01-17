4c0854c485379b3631e9fdc68875e6e4360a0ea1
insert-ctrl <59210670+insert-ctrl@users.noreply.github.com>
2020-01-16 17:46:41 -0800

    Create mod.py

---
+++ cogs/mod.py
@@ -0,0 +1,58 @@
+from discord.ext import commands
+import discord
+
+class Mod(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+    @commands.command(
+        name='kick',
+        description='Kick user.',
+        aliases=[]
+    )
+    async def kick(self, ctx, member: discord.Member):
+        if ctx.author.id == 640203987437748246:
+            guild=ctx.guild
+            try:
+                await member.kick()
+                await ctx.send('{} has been kicked by {}.'.format(member, ctx.author))
+            except Exception as e:
+                await ctx.send(f'```{e}```')
+        else:
+            return
+
+    @commands.command(
+        name='ban',
+        description='Ban user',
+        aliases=[]
+    )
+    async def ban(self, ctx, member:discord.Member):
+        if ctx.author.id == 640203987437748246:
+            guild=ctx.guild
+            try:
+                await member.ban()
+                await ctx.send('{} has been banned.'.format(member))
+            except Exception as e:
+                await ctx.send(f'```{e}```')
+        else:
+            return
+    
+    @commands.command(
+        name='unban',
+        description='Unban user (doenst work)',
+        aliases=[]
+    )
+    async def unban(self, ctx, member:discord.Member):
+        if ctx.author.id == 640203987437748246:
+            guild=ctx.guild
+            try:
+                await member.unban()
+                await ctx.send('{} has been unbanned.'.format(member))
+            except Exception as e:
+                await ctx.send('```{}```'.format(e))
+        else:
+            return
+
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
