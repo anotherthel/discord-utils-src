ab3eccb406369fd3c434c65e17df4a8ee36b8d70
insert-ctrl <59210670+insert-ctrl@users.noreply.github.com>
2020-01-16 17:45:55 -0800

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,36 +2,7 @@ from discord.ext import commands
 import discord, csv, random, asyncio
 
 
-colors = {
-  'DEFAULT': 0x000000,
-  'WHITE': 0xFFFFFF,
-  'AQUA': 0x1ABC9C,
-  'GREEN': 0x2ECC71,
-  'BLUE': 0x3498DB,
-  'PURPLE': 0x9B59B6,
-  'LUMINOUS_VIVID_PINK': 0xE91E63,
-  'GOLD': 0xF1C40F,
-  'ORANGE': 0xE67E22,
-  'RED': 0xE74C3C,
-  'GREY': 0x95A5A6,
-  'NAVY': 0x34495E,
-  'DARK_AQUA': 0x11806A,
-  'DARK_GREEN': 0x1F8B4C,
-  'DARK_BLUE': 0x206694,
-  'DARK_PURPLE': 0x71368A,
-  'DARK_VIVID_PINK': 0xAD1457,
-  'DARK_GOLD': 0xC27C0E,
-  'DARK_ORANGE': 0xA84300,
-  'DARK_RED': 0x992D22,
-  'DARK_GREY': 0x979C9F,
-  'DARKER_GREY': 0x7F8C8D,
-  'LIGHT_GREY': 0xBCC0C0,
-  'DARK_NAVY': 0x2C3E50,
-  'BLURPLE': 0x7289DA,
-  'GREYPLE': 0x99AAB5,
-  'DARK_BUT_NOT_BLACK': 0x2C2F33,
-  'NOT_QUITE_BLACK': 0x23272A
-}
+
 
 colors=[0x000000,0xFFFFFF,0x1ABC9C,0x2ECC71,0x3498DB,0x9B59B6,0xE91E63,0xF1C40F,0xE67E22,0xE74C3C,0x95A5A6,0x34495E,0x34495E,0x11806A,0x11806A,0x1F8B4C,0x206694,0x71368A,0xAD1457,0xC27C0E,0xA84300,0x7289DA0,0x99AAB5]
 
@@ -71,8 +42,8 @@ class Misc(commands.Cog):
         quoted=list(readed)
         r=random.randint(0, 178)
         sent = await ctx.send('```{}```'.format(quoted[r]))
-        await sent.add_reaction(emoji='👍')
-        await sent.add_reaction(emoji='👎')
+        await sent.add_reaction(emoji='\U0001f44d')
+        await sent.add_reaction(emoji='\U0001f44e')
 
 
         
@@ -133,8 +104,8 @@ class Misc(commands.Cog):
             msg=await ctx.send('Setting status...')
             await asyncio.sleep(0.75)
             await msg.edit(content='Status changed to:\nStatus: `discord.Status.online`\nActivity: `{}`'.format(game))
-            await msg.add_reaction(emoji='👍')
-            await msg.add_reaction(emoji='👎')
+            await msg.add_reaction(emoji='\U0001f44d')
+            await msg.add_reaction(emoji='\U0001f44e')
         else:
             await ctx.send('`set_status` is an owner only command.')
             return
