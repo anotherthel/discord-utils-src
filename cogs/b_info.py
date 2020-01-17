17effda680261a8019d3638bd45f6f6627e32a55
insert-ctrl <59210670+insert-ctrl@users.noreply.github.com>
2020-01-16 21:22:54 -0800

    Update b_info.py

--- cogs/b_info.py
+++ cogs/b_info.py
@@ -20,16 +20,22 @@ class Stuff(commands.Cog):
         xxx=list(xx)
         time=xxx[0]
         time = ' '.join(time)
-        embed=discord.Embed(title='thel vadam likes nothing jr.', description='(rewrite version w/ cogs)\n*This bot is in beta, so expect a few bugs... DM Thel \'Vadam likes nothing#1359 with any issues.*', color=0x000000)
+        embed=discord.Embed(title='thel vadam likes nothing jr.', color=0x000000)
         url=self.bot.user.avatar_url
+        embed.set_author(
+            name=ctx.message.author.name,
+            icon_url=url
+        )
         embed.set_thumbnail(url=url)
         users = 0
         for guild in self.bot.guilds:
             users += len(guild.members)
-        embed.add_field(name='__Stats__', value='**discord.py version:** {}\n**Ping time (last):** {}ms\n**Num of guilds:** {}\n**Users:** {}\n**Commands:** 27'.format(discord.__version__, time, len(self.bot.guilds), users), inline=True)
+        commands = 0
+        for command in self.bot.commands:
+            commands += 1
+        embed.add_field(name='__Stats__', value='**discord.py version:** {}\n**Ping time (last):** {}ms\n**Num of guilds:** {}\n**Users:** {}\n**Commands:** {}'.format(discord.__version__, time, len(self.bot.guilds), users, commands), inline=True)
         embed.add_field(name='__Created by:__', value='Thel Vadam likes nothing', inline=True)
-        embed.add_field(name='Owner ID:', value='{}'.format(self.bot.owner_id))
-        embed.add_field(name='Links:', value='[**invite**](https://discordapp.com/api/oauth2/authorize?client_id=665674407611727915&permissions=8&scope=bot)  | [**source**](https://github.com/insert-ctrl/discord-utils-src/tree/master)')
+        embed.add_field(name='Links:', value='[**invite**](https://discordapp.com/api/oauth2/authorize?client_id=665674407611727915&permissions=8&scope=bot)  **|** [**source**](https://github.com/insert-ctrl/discord-utils-src/tree/master)', inline=False)
         embed.set_footer(text='ID: {} | Made by Thel Vadam likes nothing#1359 | Made using repl.it'.format(self.bot.user.id))
         sended=await ctx.send(embed=embed)
         await sended.add_reaction(emoji='\U0001f44d')
@@ -49,11 +55,25 @@ class Stuff(commands.Cog):
                 bots+=1
             else:
                 humans+=1
+        online=0
+        offline=0
+        dnd=0
+        idle=0
+        for member in ctx.guild.members:
+            if member.status == discord.Status.online:
+                online += 1
+            elif member.status == discord.Status.offline:
+                offline += 1
+            elif member.status == discord.Status.dnd:
+                dnd += 1
+            elif member.status == discord.Status.idle:
+                idle += 1
         embed=discord.Embed(title='__{}__'.format(ctx.guild), color=0x000000)
         embed.set_thumbnail(url=ctx.guild.icon_url)
         embed.add_field(name='__Stats:__', value='**ID:** {}\n**Created:** {} (UTC)'.format(ctx.guild.id, ctx.guild.created_at), inline=False)
         embed.add_field(name='__Members__', value='**Members:** {}\n**Humans:** {}\n**Bots:** {}'.format(len(ctx.guild.members), humans, bots), inline=True)
-        embed.add_field(name='Number of roles:', value='{}'.format(len(ctx.guild.roles)), inline=True)
+        embed.add_field(name='__Online__', value='**Online:** {}\n**Offline:** {}\n**Dnd:** {}\n**Idle:** {}'.format(online, offline, dnd, idle), inline=True)
+        embed.add_field(name='Number of roles:', value='{}'.format(len(ctx.guild.roles)), inline=False)
         embed.add_field(name='__Channels__', value='**Text:** {}\n**Voice:** {}'.format(len(ctx.guild.text_channels), len(ctx.guild.voice_channels)), inline=False)
         embed.add_field(name='__Max members__:', value='{}'.format(ctx.guild.max_members), inline=False)
         embed.add_field(name='__Region:__', value='{}'.format(ctx.guild.region), inline=False)
@@ -76,6 +96,14 @@ class Stuff(commands.Cog):
     async def send_prefixes(self, ctx):
         prefixes = ['ut.', '[[']
         await ctx.send('**discord utils prefixes**\n{}'.format(prefixes))
+
+    @commands.command(
+        name='about',
+        description='View bot description.',
+        aliases=['__a__']
+    )
+    async def about(self, ctx):
+        await ctx.send('Coming soon.')
 
 
 
