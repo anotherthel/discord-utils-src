f083329eba93902de8ec526f9d83e9c58b0ae861
thel vadam likes nothing <59210670+insert-ctrl@users.noreply.github.com>
2020-01-19 20:34:55 -0800

    Update b_info.py

--- cogs/b_info.py
+++ cogs/b_info.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
-import discord, time, csv
+import discord, time, csv, discord.utils
 
 
 
 
 
-class Stuff(commands.Cog):
+class Stuff(commands.Cog, name='Info (various)'):
     def __init__(self, bot):
         self.bot=bot
 
@@ -15,6 +15,7 @@ class Stuff(commands.Cog):
         aliases=['ii']
     )
     async def testing(self, ctx):
+        user = self.bot.get_user(self.bot.owner_id)
         x=open('times.csv')
         xx=csv.reader(x)
         xxx=list(xx)
@@ -48,9 +49,10 @@ class Stuff(commands.Cog):
             latlat.pop()
         lat = ''.join(latlat)
         embed.add_field(name='__Stats__', value='**discord.py version:** {}\n**Avg ping time:** {}ms\n**Websocket latency:** {}ms\n**Num of guilds:** {}\n**Users:** {}\n**Commands:** {}'.format(discord.__version__,  _time, lat, len(self.bot.guilds), users, commands), inline=True)
-        embed.add_field(name='__Created by:__', value='Thel Vadam likes nothing', inline=True)
+        embed.add_field(name='__Code__', value='**Lines:** 720\n**Files:** 15\n**Cogs:** {}\n**Functions:** 47'.format(len(self.bot.cogs)), inline=True)
+        embed.add_field(name='__Created by:__', value=user, inline=False)
         embed.add_field(name='Links:', value='[**invite**](https://discordapp.com/api/oauth2/authorize?client_id=665674407611727915&permissions=8&scope=bot)  **|** [**source**](https://github.com/insert-ctrl/discord-utils-src/tree/master)', inline=False)
-        embed.set_footer(text='ID: {} | Made by Thel Vadam likes nothing#1359 | Made using repl.it'.format(self.bot.user.id))
+        embed.set_footer(text='ID: {} | Made by {} | Made using repl.it'.format(self.bot.user.id, user))
         sended=await ctx.send(embed=embed)
         await sended.add_reaction(emoji='\U0001f44d')
         await sended.add_reaction(emoji='\U0001f44e')
@@ -83,10 +85,9 @@ class Stuff(commands.Cog):
                 idle += 1
         embed=discord.Embed(title='__{}__'.format(ctx.guild), color=0x000000)
         embed.set_thumbnail(url=ctx.guild.icon_url)
-        embed.add_field(name='__Stats:__', value='**ID:** {}\n**Created:** {} (UTC)'.format(ctx.guild.id, ctx.guild.created_at), inline=False)
+        embed.add_field(name='__General:__', value='**ID:** {}\n**Created:** {} (UTC)\n**Owner:** {}\n**Roles:** {}'.format(ctx.guild.id, ctx.guild.created_at, ctx.guild.owner, len(ctx.guild.roles)), inline=False)
         embed.add_field(name='__Members__', value='**Members:** {}\n**Humans:** {}\n**Bots:** {}'.format(len(ctx.guild.members), humans, bots), inline=True)
         embed.add_field(name='__Online__', value='**Online:** {}\n**Offline:** {}\n**Dnd:** {}\n**Idle:** {}'.format(online, offline, dnd, idle), inline=True)
-        embed.add_field(name='Number of roles:', value='{}'.format(len(ctx.guild.roles)), inline=False)
         embed.add_field(name='__Channels__', value='**Text:** {}\n**Voice:** {}'.format(len(ctx.guild.text_channels), len(ctx.guild.voice_channels)), inline=False)
         embed.add_field(name='__Max members__:', value='{}'.format(ctx.guild.max_members), inline=False)
         embed.add_field(name='__Region:__', value='{}'.format(ctx.guild.region), inline=False)
@@ -116,7 +117,13 @@ class Stuff(commands.Cog):
         aliases=['__a__']
     )
     async def about(self, ctx):
-        await ctx.send('Coming soon.')
+        owner = self.bot.get_user(self.bot.owner_id)
+        embed=discord.Embed(title='About thel vadam likes nothing jr#1359', color=0x000000)
+        embed.add_field(name='Purpose:', value='thel vadam likes nothing jr\'s purpose is to be fun discord bot with some basic utility purposes such as kick, ban, etc. He also has some fun/misc features such as getting the binary code for whatever number or char, same with ascii. (tho the unicode part isnt working). thel vadam likes nothing jr is still in beta, so there are probably some bugs. DM his owner if you find any bugs.', inline=False)
+        embed.set_footer(text='Made by {}'.format(owner))
+        await ctx.send(embed=embed)
+
+
 
 
 
