6ff1fd10722c90555491fe57180e77366ed71ffd
thel vadam likes nothing <59210670+insert-ctrl@users.noreply.github.com>
2020-01-25 22:34:32 -0800

    Update b_info.py

--- cogs/b_info.py
+++ cogs/b_info.py
@@ -5,7 +5,7 @@ import discord, time, csv, discord.utils
 
 
 
-class Stuff(commands.Cog, name='Info (various)'):
+class Stuff(commands.Cog, name='Info'):
     def __init__(self, bot):
         self.bot=bot
 
@@ -31,10 +31,11 @@ class Stuff(commands.Cog, name='Info (various)'):
         for i in range(len(time)-4):
             time.pop()
         _time = ''.join(time)
-        embed=discord.Embed(title='thel vadam likes nothing jr.', color=0x000000)
+        embed=discord.Embed(title='', color=0x000000)
         url=self.bot.user.avatar_url
+        user = self.bot.get_user(665674407611727915)
         embed.set_author(
-            name=embed.author.name,
+            name=user.name,
             icon_url=url
         )
         embed.set_thumbnail(url=url)
@@ -48,11 +49,11 @@ class Stuff(commands.Cog, name='Info (various)'):
         for i in range(len(latlat)-2):
             latlat.pop()
         lat = ''.join(latlat)
-        embed.add_field(name='__Stats__', value='**discord.py version:** {}\n**Avg ping time:** {}ms\n**Websocket latency:** {}ms\n**Num of guilds:** {}\n**Users:** {}\n**Commands:** {}'.format(discord.__version__,  _time, lat, len(self.bot.guilds), users, commands), inline=True)
+        embed.add_field(name='__Stats__', value='**discord.py version:** {}\n**Avg ping time:** {}ms\n**Websocket latency:** {}ms\n**Num of guilds:** {}\n**Users:** {}\n**Commands:** {}\n**Prefixes:** `ut`, `;;`, `?!`, `ut.`\n**Cooldown:** 5s'.format(discord.__version__,  _time, lat, len(self.bot.guilds), users, commands), inline=True)
         embed.add_field(name='__Code__', value='**Lines:** 720\n**Files:** 15\n**Cogs:** {}\n**Functions:** 47'.format(len(self.bot.cogs)), inline=True)
         embed.add_field(name='__Created by:__', value=user, inline=False)
         embed.add_field(name='Links:', value='[**invite**](https://discordapp.com/api/oauth2/authorize?client_id=665674407611727915&permissions=8&scope=bot)  **|** [**source**](https://github.com/insert-ctrl/discord-utils-src/tree/master)', inline=False)
-        embed.set_footer(text='ID: {} | Made by {} | Made using repl.it'.format(self.bot.user.id, user))
+        embed.set_footer(text='ID: {} | Made by {} | Made using repl.it | Playing uthelp'.format(self.bot.user.id, user))
         sended=await ctx.send(embed=embed)
         await sended.add_reaction(emoji='\U0001f44d')
         await sended.add_reaction(emoji='\U0001f44e')
@@ -119,9 +120,86 @@ class Stuff(commands.Cog, name='Info (various)'):
     async def about(self, ctx):
         owner = self.bot.get_user(self.bot.owner_id)
         embed=discord.Embed(title='About thel vadam likes nothing jr#1359', color=0x000000)
-        embed.add_field(name='Purpose:', value='thel vadam likes nothing jr\'s purpose is to be fun discord bot with some basic utility purposes such as kick, ban, etc. He also has some fun/misc features such as getting the binary code for whatever number or char, same with ascii. (tho the unicode part isnt working). thel vadam likes nothing jr is still in beta, so there are probably some bugs. DM his owner if you find any bugs.', inline=False)
+        embed.add_field(name='Purpose:', value='thel vadam likes nothing jr\'s purpose is to be fun discord bot with some basic utility purposes such as kick, ban, etc. He also has some fun/misc features such as getting the binary code for whatever number or char, same with ascii. (tho the unicode part isnt working). thel vadam likes nothing jr is still in beta, so there are probably some bugs. DM his owner if you find any bugs.\nPlaying ut._help for help.', inline=False)
         embed.set_footer(text='Made by {}'.format(owner))
         await ctx.send(embed=embed)
+
+    @commands.command(
+        name='help'
+    )
+    async def _help_(self, ctx, comm='all'):
+        if comm == 'all':
+            embed=discord.Embed(title='thel vadam likes nothing jr.\'s help page', color=0x000000)
+            cogs = [c for c in self.bot.cogs.keys()]
+            for cog in cogs:
+            # Get a list of all commands under each cog
+
+                cog_commands = self.bot.get_cog(cog).get_commands()
+                commands_list = ''
+                for comm in cog_commands:
+                    commands_list += f'`{comm.name}` '
+
+                # Add the cog's details to the embed.
+
+                embed.add_field(
+                    name=cog,
+                    value=commands_list,
+                    inline=False
+                )
+            await ctx.send(embed=embed)
+        else:
+            cogs = [c for c in self.bot.cogs.keys()]
+            for cog in cogs:
+                commands_list = self.bot.get_cog(cog).get_commands()
+                for comm_ in commands_list:
+                    if comm_.name == comm:
+                        embed=discord.Embed(title='`{}`'.format(comm_.name), description='{}'.format(comm_.description), color=0x000000)
+                        if len(comm_.aliases) > 0:
+                            embed.add_field(name='Aliases', value='`{}`'.format('|'.join(comm_.aliases)), inline=False)
+                        await ctx.send(embed=embed)
+                        return
+            await ctx.send('Command not found.')
+    
+    @commands.command(
+        name='module',
+        description='View module info (on module oc)',
+        aliases=['cog']
+    )
+    async def _cogs(self, ctx, module='all'):
+        cogs = [c for c in self.bot.cogs.keys()]
+        if module == 'all':
+            embed=discord.Embed(
+                title='',
+                color=0x000000
+            )
+            all_ = ''
+            for cog in cogs:
+                all_ += '`{}`\n'.format(cog)
+            embed.add_field(
+                name='Modules:',
+                value=all_
+            )
+            await ctx.send(embed=embed)
+            
+        else:
+            if module in cogs:
+                embed=discord.Embed(title=module, color=0x000000)
+                comm_list = self.bot.get_cog(cogs[cogs.index(module)]).get_commands()
+
+                text = '```'
+
+                for comm in comm_list:
+                    text += '{}\n'.format(comm)
+                text += '```'
+                embed.add_field(name='Commands:',value=text, inline=False)
+                await ctx.send(embed=embed)
+                return
+            await ctx.send('Module not found. Remember: module names are case sensitive.')
+
+
+
+        
+
 
 
 
